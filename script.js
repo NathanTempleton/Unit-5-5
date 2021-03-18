5c0ffe756396d9a043d003de57f512e7ec6b5466
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-18 13:45:22 +0000

    Added comments and fixed up errors in javascript and css

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// I set the length to 0 so it can be changed later on by the users input.
 let length = 0
+let area = 0
+let width = 0
+// This will make the document look for the element button to be clicked, which will cause math to activate.
 document.getElementById('button').addEventListener('click', math)
 
-function math() {
+// This function will do the math for the square by getting the users input for length, doing the math, and then displaying the area through an alert.
+function math () {
   length = document.getElementById('input').value
   width = length
   area = length * width
